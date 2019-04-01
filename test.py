a6f4c60a8ce1602e120d871085689608042fcccb
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2019-04-01 14:37:38 -0500

    Bugfixes

--- test.py
+++ test.py
@@ -16,22 +16,33 @@ landscape = np.load("../../data/landscape-diffuse-150.npy")
 #landscape[1::100, :] = 1
 #landscape[2::100, :] = 1
 
-nsf = NavBySceneFamiliarity(landscape, (30, 20), 2.0, sensor_pixel_dimensions = [2, 4], n_test_angles = 180, n_sensor_levels = 6,
+nsf = NavBySceneFamiliarity(landscape, (40, 2), 2.0, sensor_pixel_dimensions = [2, 4], n_test_angles = 180, n_sensor_levels = 6,
                             max_distance_to_training_path = 12)
 
-x = np.linspace(50, 450, 300)
-l = 400
-path = np.vstack((x, x - 0.5 * l * 0.05 * np.sin((x - 0.5 * l - np.min(x)) * np.pi / (0.5 * l)))).T
+def sin_training_path(curveness, start_x, l, arclen = 2.0):
+    # Assume derivative never goes above 4
+    x = np.linspace(start_x, start_x + l, 4 * np.floor(l / arclen))
+    y = x - 0.5 * l * curveness * np.sin((x - 0.5 * l - start_x) * np.pi / (0.5 * l))
+    dists = np.sqrt((x[1:] - x[:-1])**2 + (y[1:] - y[:-1])**2)
+
+    i = np.searchsorted(np.cumsum(dists), arclen * np.arange(np.floor(np.sum(dists) / arclen)))
+
+    path = np.vstack((x[i], y[i])).T
+
+    return path
+
+path = sin_training_path(0.0, 100, 300, arclen = 1.)
+print(len(path))
 #path = np.vstack((x, np.full(len(x), 185.))).T
 
 nsf.train_from_path(path)
 
-nsf.position = path[150] + [1.0, -1.0]
+nsf.position = path[0] + [3.0, -2.0]
 nsf.angle = 0.
 
-fig, anim = nsf.animate(frames = 200)
-anim.save("test-curveness0-1.mp4")
+#fig, anim = nsf.animate(frames = 20)
+#anim.save("test-curveness0-1.mp4")
+#plt.show(fig)
 
-fig = nsf.quiver_plot(n_box = 8)
-
+fig = nsf.quiver_plot(n_box = 10, max_distance = 20.0)
 plt.show(fig)
